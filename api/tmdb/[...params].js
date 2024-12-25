45d372bb83506614c1455843e0ed29ec858781da
milindkusahu <milindsahu2000@gmail.com>
2024-12-25 12:34:23 +0530

    Added: Access-Control-Allow-Headers header

--- api/tmdb/[...params].js
+++ api/tmdb/[...params].js
@@ -10,6 +10,7 @@ export default async function handler(req, res) {
   // Add CORS headers
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Authorization");
 
   if (req.method === "OPTIONS") {
     res.status(200).end();
